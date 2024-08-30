4668502df0a8650801464a2ba5bde2ade47a5ecf
John Lalor <john.lalor@nd.edu>
2024-08-30 10:04:03 -0400

    new versions of plots

--- CostComparison.py
+++ CostComparison.py
@@ -1,32 +1,92 @@
 import matplotlib.pyplot as plt
 
+
+# This file generates what is currently figure 2 in the tacl paper.
+# specifically, input and output token cost across LLMs
+
 # Data
 models = ['GPT-3.5', 'GPT-4', 'GPT-4 Omni', 'Llama-2', 'Llama-3', 'Llama-3.1']
 input_costs = [116.7109, 2337.9695, 390.5797, 50.5953, 50.5323, 250.2109]
 output_costs = [21.4, 1012.11, 296.63, 41.0691, 38.9646, 20.7449]
 
 # Converting costs to smaller units for clarity
-input_costs_usd = [x / 1 for x in input_costs]
-output_costs_usd = [x / 1 for x in output_costs]
+# JL: set the values as their true values, we deal with scaling later
+input_costs_usd = [x * 1000 for x in input_costs]
+output_costs_usd = [x * 1000 for x in output_costs]
 
 # Plot
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(6, 4))
+
+
+# Formatting
+# source: https://www.practicaldatascience.org/notebooks/class_5/week_5/46_making_plots_pretty.html
+
+# Define font sizes
+SIZE_DEFAULT = 12
+SIZE_LARGE = 20
+plt.rc("font", family="Roboto")  # controls default font
+plt.rc("font", weight="normal")  # controls default font
+plt.rc("font", size=SIZE_DEFAULT)  # controls default text sizes
+plt.rc("axes", titlesize=SIZE_LARGE)  # fontsize of the axes title
+plt.rc("axes", labelsize=SIZE_LARGE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SIZE_LARGE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SIZE_LARGE)  # fontsize of the tick labels
+
+
 ax.plot(models, input_costs_usd, marker='o', linestyle='-', color='steelblue', label='Input')
 ax.plot(models, output_costs_usd, marker='o', linestyle='--', color='darkorange', label='Output')
 
+# Redo legend
+
+# Plot the cost text
+ax.text(
+    5 * 1.02,
+    input_costs_usd[-1],
+    "Inputs",
+    color="steelblue",
+    fontweight="bold",
+    horizontalalignment="left",
+    verticalalignment="center",
+)
+
+ax.text(
+    5 * 1.02,
+    output_costs_usd[-1],
+    "Outputs",
+    color="darkorange",
+    fontweight="bold",
+    horizontalalignment="left",
+    verticalalignment="center",
+)
+
+
+# Hide the all but the bottom spines (axis lines)
+ax.spines["right"].set_visible(False)
+ax.spines["left"].set_visible(False)
+ax.spines["top"].set_visible(False)
+
+# Only show ticks on the left and bottom spines
+ax.yaxis.set_ticks_position("left")
+ax.xaxis.set_ticks_position("bottom")
+ax.spines["bottom"].set_bounds(0, len(models))
+
+
 # Title and labels
 #ax.set_title('Comparison of Input and Output Costs for Various Language Models', fontsize=10)
-ax.set_ylabel('Cost in Thousands (USD)', fontsize=10, fontweight='bold')
-ax.set_xlabel('Model', fontsize=10, fontweight='bold')
+ax.set_ylabel('Cost (log USD)', fontweight='bold')
+ax.set_xlabel('Model', fontweight='bold')
 
 # Set y-axis limit
-ax.set_ylim(0, 2500)
+#ax.set_ylim(0, 2500)
+ax.set_yscale('log')
 
-# Legend
-ax.legend()
+# Remove Legend
+#ax.legend()
 
 # Show plot
-plt.xticks(rotation=0, fontsize=8)  # Rotate model names for better visibility
-plt.yticks(fontsize=8)
-plt.grid(True)
-plt.show()
+plt.xticks()  # Rotate model names for better visibility
+plt.yticks()
+# remove grid
+#plt.grid(True)
+plt.savefig('plots/costv2.png', dpi=1000)
+#plt.show()
--- Interactionplots.py
+++ Interactionplots.py
@@ -3,116 +3,191 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load data
-data_path = 'Data/1-ShotHuman-LLMslong_data.xlsx'  
 
-data_long = pd.read_excel(data_path)
+## STEP 1: generate what is currently figure 4 in TACL 
+# two-panel plot with one legend at the bottom
 
-# Convert columns to categorical
-data_long['Assessor'] = pd.Categorical(data_long['Assessor'], categories=["GPT3.5", "GPT4", "GPT4o", "Llama2", "Llama3", "Llama3.1"])
-data_long['Prompt_ID'] = pd.Categorical(data_long['Prompt_ID'])
-data_long['Prompt_Type'] = pd.Categorical(data_long['Prompt_Type'])
+def generate_fig4():
 
-# Simplify group variable
-data_long['Group'] = data_long['Prompt_Type'].astype(str)
+    # Load data
+    data_path = 'Data/1-ShotHuman-LLMslong_data.xlsx'  
+    data_long = pd.read_excel(data_path)
 
-# Remove duplicates if any
-data_long.drop_duplicates(inplace=True)
+    # Convert columns to categorical
+    data_long['Assessor'] = pd.Categorical(data_long['Assessor'], categories=["GPT3.5", "GPT4", "GPT4o", "Llama2", "Llama3", "Llama3.1"])
+    data_long['Prompt_ID'] = pd.Categorical(data_long['Prompt_ID'])
+    data_long['Prompt_Type'] = pd.Categorical(data_long['Prompt_Type'])
 
-# Reset the index of df
-data_long.reset_index(drop=True, inplace=True)
+    # Simplify group variable
+    data_long['Group'] = data_long['Prompt_Type'].astype(str)
 
-# Define a custom color palette using color names
-custom_palette = {
-    "GPT3.5": "steelblue",  
-    "GPT4": "steelblue",   
-    "GPT4o": "steelblue",   
-    "Llama2": "darkorange",   
-    "Llama3": "darkorange",   
-    "Llama3.1": "darkorange", 
-    "Human": "olive"        
-}
+    # Remove duplicates if any
+    data_long.drop_duplicates(inplace=True)
 
-# Custom settings for the plots
-sns.set(style="whitegrid", palette="muted")
-markers = ["o", "^", "s", "p", "*", "h", "D"]
-linestyles = ["-", "--", "-.", ":", "-", "-.", ":"]
+    # Reset the index of df
+    data_long.reset_index(drop=True, inplace=True)
 
-# Hue order for the legend
-hue_order = ["GPT3.5", "GPT4", "GPT4o", "Llama2", "Llama3", "Llama3.1", "Human"]
+    # Define a custom color palette using color names
+    custom_palette = {
+        "GPT3.5": "steelblue",  
+        "GPT4": "steelblue",   
+        "GPT4o": "steelblue",   
+        "Llama2": "darkorange",   
+        "Llama3": "darkorange",   
+        "Llama3.1": "darkorange", 
+        "Human": "olive"        
+    }
 
-# Figure 3a: Interaction between Assessor and Respondent
-g = sns.catplot(data=data_long, kind="point", x='Assessor', y='Score', hue='Respondent', hue_order=hue_order, palette=custom_palette, height=4, aspect=1.5,
-                markers=markers, linestyles=linestyles, linewidth=1.3)
-plt.grid(True, which='both', color='gray', linewidth=0.3)
-g.set_axis_labels('Assessment Model', 'Score')
-g.set_xticklabels(rotation=45, fontsize=9)
-g.add_legend(title='Respondent')
-g.tight_layout()
-g.savefig('plots/1shotassresp.png', dpi=1000)
-plt.show()
+    # Custom settings for the plots
+    sns.set(style="whitegrid", palette="muted")
+    markers = ["o", "^", "s", "p", "*", "h", "D"]
+    linestyles = ["-", "--", "-.", ":", "-", "-.", ":"]
 
-# Figure 3b: Interaction between Respondent and Prompt Type
-g = sns.catplot(data=data_long, kind="point", x='Prompt_Type', y='Score', hue='Respondent', hue_order=hue_order, palette=custom_palette, height=4, aspect=1.5,
-                markers=markers, linestyles=linestyles, linewidth=1.3)
-plt.grid(True, which='both', linestyle='-', color='gray', linewidth=0.3)
-g.set_axis_labels('Prompt Type', 'Score')
-g.set_xticklabels(rotation=45, fontsize=9)
-g.add_legend(title='Respondent')
-g.tight_layout()
-g.savefig('plots/1shotpromptresp.png', dpi=1000)
-plt.show()
+    # Hue order for the legend
+    hue_order = ["GPT3.5", "GPT4", "GPT4o", "Llama2", "Llama3", "Llama3.1", "Human"]
+
+    
+    # Figure 3a: Interaction between Assessor and Respondent
+    sns.set_style("ticks")
+
+    
+    g = sns.PairGrid(
+        data_long, 
+        y_vars=["Score"],
+        x_vars=["Assessor", "Prompt_Type"],
+        hue='Respondent', 
+        hue_order=hue_order, 
+        palette=custom_palette, 
+        height=4, 
+        aspect=1.5
+    )
+    g.map(
+        sns.pointplot,
+        markers=markers, 
+        linestyles=linestyles, 
+        linewidth=1.3
+    )
+
+    #plt.grid(True, which='both', color='gray', linewidth=0.3)
+    g.add_legend(title='Respondent')
+    sns.move_legend(
+        g, "lower center",
+        bbox_to_anchor=(.5, 1), ncol=7, title='Respondent', frameon=False,
+    )
+
+    g.savefig('plots/1shotplot.png', dpi=1000)
+    #plt.show()
+
+def generate_fig5():
+    ###FAIRNESS/BIAS PLOTS###
+    # Load data
+    data_path = 'Data/FCE-long_data_with_Family.xlsx'
+    data_long = pd.read_excel(data_path)
+
+    # Convert columns to categorical
+    data_long['Age Group'] = pd.Categorical(data_long['Age Group'])
+    data_long['Assessor'] = pd.Categorical(data_long['Assessor'], categories=["GPT3.5", "GPT4", "GPT4o", "Llama2", "Llama3", "Llama3.1"])
+    data_long['Prompt_ID'] = pd.Categorical(data_long['Prompt_ID'])
+    data_long['Prompt_Type'] = pd.Categorical(data_long['Prompt_Type'])
+    data_long['Group'] = data_long['Prompt_Type'].astype(str)
+
+    # Reset the index of df
+    data_long.reset_index(drop=True, inplace=True)
+
+    # Custom palette, markers
+    custom_palette = {
+        "Old": "darkorange",
+        "Young": "steelblue",
+    }
+
+    # Figure 4a: Interaction across Assessment Models and Age Groups
+    sns.set_style("ticks")
+
+    g = sns.catplot(
+        data=data_long, 
+        x='Assessor', 
+        y='∆ Score', 
+        hue='Age Group', 
+        kind='point',
+        palette=custom_palette, 
+        height=4, 
+        aspect=1.25,
+        linestyles=["-", "--"], 
+        markers=["o", "^"],
+        linewidth=1.3
+    )
+    
+    #g.add_legend(title='Age Group')
+    sns.move_legend(
+        g, "lower center",
+        bbox_to_anchor=(.5, 1), ncol=7, frameon=False,
+    )
+
+    # Customize the plot further 
+    g.set_axis_labels('Assessment Model', '∆ Score')
+    #g.set_xticklabels(fontsize=9)
+    g.set(ylim=(-0.1, 0.15)) 
+    g.set(yticks=[-0.1,-0.05, 0, 0.05, 0.10, 0.15])
+    g.savefig('plots/interactionsAge.png', dpi=1000)
+    #plt.show()
+
+    # Custom palette, markers
+    custom_palette = {
+        "Non-Asian": "darkorange",
+        "Asian": "steelblue",
+    }
+    
+    g = sns.catplot(
+        data=data_long, 
+        x='Assessor', 
+        y='∆ Score', 
+        hue='Race', 
+        kind='point',
+        palette=custom_palette, 
+        height=4, 
+        aspect=1.25,
+        linestyles=["-", "--"], 
+        markers=["o", "^"],
+        linewidth=1.3
+    )
+    
+    #g.add_legend(title='Race')
+    sns.move_legend(
+        g, "lower center",
+        bbox_to_anchor=(.5, 1), ncol=7, frameon=False,
+    )
+
+    # Customize the plot further 
+    g.set_axis_labels('Assessment Model', '∆ Score')
+    #g.set_xticklabels(fontsize=9)
+    g.set(ylim=(-0.1, 0.15)) 
+    g.set(yticks=[-0.1,-0.05, 0, 0.05, 0.10, 0.15])
+    g.savefig('plots/interactionsRace.png', dpi=1000)
+    #plt.show()
+
+    # Figure 5:Interaction across Prompt Types
+    # JL: I've commented out the below as we aren't using it at the moment...
+    """
+    g = sns.catplot(data=data_long, x='Assessor', y='∆ Score', hue='Age Group', col='Prompt_Type', kind='point',
+                    palette=custom_palette, height=4, aspect=1.5, markers=markers, linestyles=linestyles, linewidth=1.3)
+    # increase the thickness of the lines
+    for ax in g.axes.flatten():
+        ax.grid(True, which='both', linestyle='-', color='gray', linewidth=0.3)
+        for line in ax.get_lines():
+            line.set_linewidth(1.3)
+    g.fig.suptitle('Interaction Effect between Assessment Models, Age, and Prompt Types', y=0.95, fontsize=12)
+    g.set_axis_labels('Assessment Model', '∆ Score')
+    g.set_xticklabels(rotation=90, fontsize=9)
+    g.set(ylim=(-0.2, 0.3))
+    g.set(yticks=[-0.2,-0.1, 0, 0.1, 0.2, 0.3])
+    g.fig.set_size_inches(14, 4)
+    g.fig.tight_layout(rect=[0, 0, 0.95, 0.95])
+    # g.add_legend(title="Age_Group")
+    g.savefig('plots/interactionsPrompt.png', dpi=1000)
+    #plt.show()
+    """
 
 
-###FAIRNESS/BIAS PLOTS###
-# Load data
-data_path = 'Data/FCE-long_data_with_Family.xlsx'
-data_long = pd.read_excel(data_path)
 
-# Convert columns to categorical
-data_long['Age Group'] = pd.Categorical(data_long['Age Group'])
-data_long['Assessor'] = pd.Categorical(data_long['Assessor'], categories=["GPT3.5", "GPT4", "GPT4o", "Llama2", "Llama3", "Llama3.1"])
-data_long['Prompt_ID'] = pd.Categorical(data_long['Prompt_ID'])
-data_long['Prompt_Type'] = pd.Categorical(data_long['Prompt_Type'])
-data_long['Group'] = data_long['Prompt_Type'].astype(str)
-
-# Reset the index of df
-data_long.reset_index(drop=True, inplace=True)
-
-# Custom palette, markers
-custom_palette = {
-    "Old": "darkorange",
-    "Young": "steelblue",
-}
-
-# Figure 4a: Interaction across Assessment Models and Age Groups
-g = sns.catplot(data=data_long, x='Assessor', y='∆ Score', hue='Age Group', kind='point',
-                palette=custom_palette, height=4, aspect=1.5, linestyles=["-", "--"], markers=["o", "^"])
-
-# Customize the plot further 
-g.set_axis_labels('Assessment Model', '∆ Score')
-g.set_xticklabels(rotation=45, fontsize=9)
-g.set(ylim=(-0.1, 0.15)) 
-g.set(yticks=[-0.1,-0.05, 0, 0.05, 0.10, 0.15])
-g.add_legend(title="Age Group")
-g.despine(left=True)
-plt.grid(True, which='both', linestyle='-', color='gray', linewidth=1.3)
-plt.show()
-
-# Figure 5:Interaction across Prompt Types
-g = sns.catplot(data=data_long, x='Assessor', y='∆ Score', hue='Age Group', col='Prompt_Type', kind='point',
-                palette=custom_palette, height=4, aspect=1.5, markers=markers, linestyles=linestyles, linewidth=1.3)
-# increase the thickness of the lines
-for ax in g.axes.flatten():
-    ax.grid(True, which='both', linestyle='-', color='gray', linewidth=0.3)
-    for line in ax.get_lines():
-        line.set_linewidth(1.3)
-g.fig.suptitle('Interaction Effect between Assessment Models, Age, and Prompt Types', y=0.95, fontsize=12)
-g.set_axis_labels('Assessment Model', '∆ Score')
-g.set_xticklabels(rotation=90, fontsize=9)
-g.set(ylim=(-0.2, 0.3))
-g.set(yticks=[-0.2,-0.1, 0, 0.1, 0.2, 0.3])
-g.fig.set_size_inches(14, 4)
-g.fig.tight_layout(rect=[0, 0, 0.95, 0.95])
-# g.add_legend(title="Age_Group")
-plt.show()
+generate_fig4()
+generate_fig5()
