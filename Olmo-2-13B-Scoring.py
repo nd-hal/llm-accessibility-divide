b34c74c1ca264e2d5c3276622c9f34f96bf7da37
John Lalor <john.lalor@nd.edu>
2025-01-30 10:34:46 -0500

    try with llamacpp

--- Olmo-2-13B-Scoring.py
+++ Olmo-2-13B-Scoring.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import time
 
-from ollama import generate
+from llama_cpp import Llama
 
 # Load and read the input files
 input_file = "./Data/1-ShotHuman_generated_data.xlsx"
@@ -18,15 +18,22 @@ if 'Olmo2-13B' not in df.columns:
     df['Olmo2-13B'] = pd.Series(dtype='float64')
 
 
+llm = Llama.from_pretrained(
+    repo_id="allenai/OLMo-2-1124-13B-Instruct-GGUF",
+    filename="*F16_0.gguf",
+    verbose=False,
+    n_gpu_layers=-1,
+    n_ctx=4096
+)
 
 system_prompt = "You are OLMo 2, a helpful and harmless AI Assistant built by the Allen Institute for AI. You are a virtual grading assistant. Directly provide a numeric score explicitly formatted as 'Score: [number]'."
 
 options = {
-        #"max_tokens": 2000,  
+        "max_tokens": 2000,  
         "temperature": 0.7,
         "top_p": 0.95,
         "frequency_penalty": 1.0,
-	"num_ctx":4096,
+	    "num_ctx":4096,
         #"stream": False,
 }
 
@@ -35,13 +42,23 @@ modelName = "olmo2:13b"
 # Function to generate score 
 def generate_score(instruction):
     try:
-        response = generate(
-            model=modelName, 
-            prompt=instruction,
-            system=system_prompt,
-            options=options
+        response = llm.create_chat_completion(
+            messages=[
+                {
+                "role": "system",
+                "content": system_prompt
+                },
+                {"role": "user",
+                 "content": instruction
+                }
+            ],
+            max_tokens= 2000,  
+            temperature= 0.7,
+            top_p= 0.95,
+            frequency_penalty= 1.0
         )
-        content = response["response"]
+
+        content = response["choices"][0]["message"]["content"]
         #print(content)
         # Extract the numeric score from the response content
         score_start = content.find("Score:") + len("Score:")
