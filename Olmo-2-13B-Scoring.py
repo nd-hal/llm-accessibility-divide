0a1dbccda5c2ce61ee149233134721c6f977a1e5
John Lalor <john.lalor@nd.edu>
2025-02-03 14:06:35 -0500

    new setup for grading

--- Olmo-2-13B-Scoring.py
+++ Olmo-2-13B-Scoring.py
@@ -6,63 +6,25 @@ from vllm import LLM, SamplingParams
 from vllm.sampling_params import GuidedDecodingParams
 
 # Load and read the input files
-input_file = "./Data/1-ShotHuman_generated_data.xlsx"
-input_file = "./Data/1-ShotHuman_scored_by_Olmo2_13B.xlsx"
-output_file = "./Data/1-ShotHuman_scored_by_Olmo2_13B_v2.xlsx"
-temp_output_file = "./Data/1-ShotHuman_generated_dataTEMP.xlsx"
-df = pd.read_excel(input_file)
-df = df[df["Olmo2-13B"].str.startswith("!") | df["Olmo2-13B"].isna()]
-
-assert(len(df["OneShotRubric"]) > 0)
-print(len(df["OneShotRubric"]))
-
-# Essays, instructions, and rubrics are in the 7th column
-instructions_column = df["OneShotRubric"]
-
-# Ensure the 'Llama3.1-70B_1-Shot' column is present and set to numeric type
-if 'Olmo2-13B' not in df.columns:
-    df['Olmo2-13B'] = pd.Series(dtype='float64')
+#input_file = "./Data/1-ShotHuman_generated_data.xlsx"
+#input_file = "./Data/1-ShotHuman_scored_by_Olmo2_13B.xlsx"
 
 
-# load the LLM model
-llm = LLM(
-    model="allenai/OLMo-2-1124-13B-Instruct",
-    max_model_len=8192,
-    rope_scaling={"factor":2,"rope_type":"linear"}
-)
-
-# Create a sampling params object.
-guided_decoding_params = GuidedDecodingParams(regex=r"Score: \d+(\.*\d*)")
-sampling_params = SamplingParams(
-    temperature=0.7, 
-    top_p=0.95,
-    frequency_penalty= 1.0,
-    max_tokens= 2000,
-    guided_decoding=guided_decoding_params
-)
-
-system_prompt = "You are a virtual grading assistant. Directly provide a numeric score explicitly formatted as 'Score: [number]'."
-
+datafolder = "./Data/"
 
 def create_conversation(prompt):
+    system_prompt = "You are a virtual grading assistant. Directly provide a numeric score explicitly formatted as 'Score: [number]'."
+
     msg = [
                 {
                 "role": "system",
                 "content": system_prompt
                 },
                 {"role": "user",
-                 "content": prompt
+                "content": prompt
                 }
     ]
     return msg
-
-conversations = [create_conversation(prompt) for prompt in instructions_column]
-
-outputs = llm.chat(
-    conversations,
-    sampling_params=sampling_params,
-    use_tqdm=True
-)
 
 
 def extract_scores(output):
@@ -80,10 +42,80 @@ def extract_scores(output):
     #return score
     return content
 
-df["Olmo2-13B"] = [extract_scores(output) for output in outputs]
 
-# Save the final df to the output file
-df.to_excel(output_file, index=False)
+def grade_model_outputs(modelName):
 
-print("Scores have been generated and saved to", output_file)
+    input_file = f"./Data/{modelName}_generated_data.xlsx"
+    output_file = f"./Data/{modelName}_graded_by_Olmo2_13B.csv"
+
+    #output_file = "./Data/1-ShotHuman_scored_by_Olmo2_13B_v2.xlsx"
+    #temp_output_file = "./Data/1-ShotHuman_generated_dataTEMP.xlsx"
+
+    df = pd.read_excel(input_file)
+    #df = df[df["Olmo2-13B"].str.startswith("!") | df["Olmo2-13B"].isna()]
+
+    assert(len(df["OneShotRubric"]) > 0)
+    print(len(df["OneShotRubric"]))
+
+    # Essays, instructions, and rubrics are in the 7th column
+    zeroShots = df["Response"]
+    oneShots = df["1-Shot Rubric"]
+
+    outputDF = df[["Prompt ID","Prompt Type","Prompt Variation","Iteration"]]
+
+    # Ensure the 'Llama3.1-70B_1-Shot' column is present and set to numeric type
+    outputDF['Olmo2-13B-zeroshot'] = pd.Series(dtype='float64')
+    outputDF['Olmo2-13B-oneshot'] = pd.Series(dtype='float64')
+
+    # load the LLM model
+    # no rope scaling for now bc it doesnt work...
+    llm = LLM(
+        model="allenai/OLMo-2-1124-13B-Instruct",
+        #max_model_len=8192,
+        #rope_scaling={"factor":2,"rope_type":"linear"}
+    )
+
+    # Create a sampling params object.
+    guided_decoding_params = GuidedDecodingParams(regex=r"Score: \d+(\.*\d*)")
+    sampling_params = SamplingParams(
+        temperature=0.7, 
+        top_p=0.95,
+        frequency_penalty= 1.0,
+        max_tokens= 2000,
+        guided_decoding=guided_decoding_params
+    )
+
+
+    conversationsZS = [create_conversation(prompt) for prompt in zeroShots]
+
+    outputs = llm.chat(
+        conversationsZS,
+        sampling_params=sampling_params,
+        use_tqdm=True
+    )
+
+    outputDF['Olmo2-13B-zeroshot'] = [extract_scores(output) for output in outputs]
+
+
+    conversationsOS = [create_conversation(prompt) for prompt in oneShots]
+
+    outputs = llm.chat(
+        conversationsOS,
+        sampling_params=sampling_params,
+        use_tqdm=True
+    )
+
+    outputDF['Olmo2-13B-oneshot'] = [extract_scores(output) for output in outputs]
+
+    # Save the final df to the output file
+    outputDF.to_csv(output_file, index=False)
+
+    print("Scores have been generated and saved to", output_file)
+
+
+print("grading Qwen2.5")
+grade_model_outputs("Qwen2.5-72B")
+
+print("grading deepseek")
+grade_model_outputs("Deepseek-R1")
 
