6281c11b159d34a62d90c0446a9cb9787df26be0
John P. Lalor <john.lalor@nd.edu>
2025-02-03 13:44:58 -0500

    update

--- Olmo-2-13B-Scoring.py
+++ Olmo-2-13B-Scoring.py
@@ -7,9 +7,14 @@ from vllm.sampling_params import GuidedDecodingParams
 
 # Load and read the input files
 input_file = "./Data/1-ShotHuman_generated_data.xlsx"
-output_file = "./Data/1-ShotHuman_scored_by_Olmo2_13B.xlsx"
+input_file = "./Data/1-ShotHuman_scored_by_Olmo2_13B.xlsx"
+output_file = "./Data/1-ShotHuman_scored_by_Olmo2_13B_v2.xlsx"
 temp_output_file = "./Data/1-ShotHuman_generated_dataTEMP.xlsx"
 df = pd.read_excel(input_file)
+df = df[df["Olmo2-13B"].str.startswith("!") | df["Olmo2-13B"].isna()]
+
+assert(len(df["OneShotRubric"]) > 0)
+print(len(df["OneShotRubric"]))
 
 # Essays, instructions, and rubrics are in the 7th column
 instructions_column = df["OneShotRubric"]
@@ -22,8 +27,8 @@ if 'Olmo2-13B' not in df.columns:
 # load the LLM model
 llm = LLM(
     model="allenai/OLMo-2-1124-13B-Instruct",
-    max_model_len=7168		,
-    rope_scaling={"factor":1.75,"rope_type":"dynamic"}
+    max_model_len=8192,
+    rope_scaling={"factor":2,"rope_type":"linear"}
 )
 
 # Create a sampling params object.
